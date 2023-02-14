e2606106d4cd61ecb963df2c00875a00d134fa76
Shoubhit Dash <shoubhit2005@gmail.com>
2023-02-14 14:15:02 +0530

    +(typeguards): add string and object

--- src/typeguards/index.ts
+++ src/typeguards/index.ts
@@ -1,3 +1,9 @@
 // deno-lint-ignore-file ban-types
 export const isFunction = (value: unknown): value is Function =>
   typeof value === "function";
+
+export const isString = (value: unknown): value is string =>
+  typeof value === "string";
+
+export const isObject = (value: unknown): value is object =>
+  typeof value === "object" && value !== null;
