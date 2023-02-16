014f8ebdf110b41876319adbdffec54748a7471a
Shoubhit Dash <shoubhit2005@gmail.com>
2023-02-16 11:38:06 +0530

    +(result): write tests and tsdoc

--- src/Result.ts
+++ src/Result.ts
@@ -5,14 +5,46 @@ export type Err<E> = { type: "err"; value: E };
 
 export type Result<T, E> = Ok<T> | Err<E>;
 
+/**
+ * Wraps a value in a Result of Ok type.
+ *
+ * @param value - The value to wrap in a Result of Ok type
+ *
+ * @example
+ * ```
+ * const result = R.ok(1);
+ * ```
+ */
 export function ok<T, E>(value: T): Result<T, E> {
   return { type: "ok", value };
 }
 
+/**
+ * Wraps a value in a Result of Err type.
+ *
+ * @param value - The value to wrap in a Result of Err type
+ *
+ * @example
+ * ```
+ * const result = R.err("error");
+ * ```
+ */
 export function err<T, E>(value: E): Result<T, E> {
   return { type: "err", value };
 }
 
+/**
+ * Checks if a value is a Result.
+ *
+ * @param value - The value to check
+ *
+ * @example
+ * ```
+ * assertEquals(R.isResult(R.ok(1)), true);
+ * assertEquals(R.isResult(R.err("error")), true);
+ * assertEquals(R.isResult(1), false);
+ * ```
+ */
 export function isResult<T, E>(value: unknown): value is Result<T, E> {
   return (
     isObject(value) &&
@@ -21,14 +53,51 @@ export function isResult<T, E>(value: unknown): value is Result<T, E> {
   );
 }
 
+/**
+ * Checks if a Result is of Ok type.
+ *
+ * @param result - The Result to check
+ *
+ * @example
+ * ```
+ * assertEquals(R.isOk(R.ok(1)), true);
+ * assertEquals(R.isOk(R.err("error")), false);
+ * ```
+ */
 export function isOk<T, E>(result: Result<T, E>): result is Ok<T> {
   return result.type === "ok";
 }
 
+/**
+ * Checks if a Result is of Err type.
+ *
+ * @param result - The Result to check
+ *
+ * @example
+ * ```
+ * assertEquals(R.isErr(R.err("error")), true);
+ * assertEquals(R.isErr(R.ok(1)), false);
+ * ```
+ */
 export function isErr<T, E>(result: Result<T, E>): result is Err<E> {
   return result.type === "err";
 }
 
+/**
+ * Wraps a value in a Result of Ok type if the value is not null or undefined. Otherwise, wraps the error in a Result of Err type.
+ *
+ * @param value - The value to wrap in a Result of Ok type if the value is not null or undefined
+ * @param error - The error to wrap in a Result of Err type if the value is null or undefined
+ *
+ * @example
+ * ```
+ * const ok = R.fromNullable(1, "error");
+ * const err = R.fromNullable(null, "error");
+ *
+ * assertEquals(ok, R.ok(1));
+ * assertEquals(err, R.err("error"));
+ * ```
+ */
 export function fromNullable<T, E>(
   value: T | null | undefined,
   error: NonNullable<E>
@@ -54,6 +123,21 @@ export function fromNullable<T, E>(
   return ok(valueOrError as NonNullable<T>);
 }
 
+/**
+ * Wraps a value in a Result of Ok type if the value is truthy. Otherwise, wraps the error in a Result of Err type.
+ *
+ * @param value - The value to wrap in a Result of Ok type if the value is truthy
+ * @param error - The error to wrap in a Result of Err type if the value is falsy
+ *
+ * @example
+ * ```
+ * const ok = R.fromFalsy("hiii", "error");
+ * const err = R.fromFalsy("", "error");
+ *
+ * assertEquals(ok, R.ok("hiii"));
+ * assertEquals(err, R.err("error"));
+ * ```
+ */
 export function fromFalsy<T, E>(
   value: T,
   error: NonNullable<E>
---
+++ src/Result_test.ts
@@ -0,0 +1,50 @@
+import { assertEquals } from "https://deno.land/std@0.177.0/testing/asserts.ts";
+
+import * as R from "./Result.ts";
+
+Deno.test("Result - ok", () => {
+  const ok = R.ok(1);
+
+  assertEquals(ok, { type: "ok", value: 1 });
+});
+
+Deno.test("Result - err", () => {
+  const err = R.err("error");
+
+  assertEquals(err, { type: "err", value: "error" });
+});
+
+Deno.test("Result - isResult", () => {
+  assertEquals(R.isResult(R.ok(1)), true);
+  assertEquals(R.isResult(R.err("error")), true);
+  assertEquals(R.isResult(1), false);
+  assertEquals(R.isResult("1"), false);
+  assertEquals(R.isResult(true), false);
+  assertEquals(R.isResult(undefined), false);
+});
+
+Deno.test("Result - isOk", () => {
+  assertEquals(R.isOk(R.ok(1)), true);
+  assertEquals(R.isOk(R.err("error")), false);
+});
+
+Deno.test("Result - isErr", () => {
+  assertEquals(R.isErr(R.ok(1)), false);
+  assertEquals(R.isErr(R.err("error")), true);
+});
+
+Deno.test("Result - fromNullable", () => {
+  assertEquals(R.fromNullable(1, "error"), R.ok(1));
+  assertEquals(R.fromNullable(null, "error"), R.err("error"));
+  assertEquals(R.fromNullable(undefined, "error"), R.err("error"));
+  assertEquals(R.fromNullable(false, "error"), R.ok(false));
+  assertEquals(R.fromNullable("", "error"), R.ok(""));
+});
+
+Deno.test("Result - fromFalsy", () => {
+  assertEquals(R.fromFalsy(1, "error"), R.ok(1));
+  assertEquals(R.fromFalsy(null, "error"), R.err("error"));
+  assertEquals(R.fromFalsy(undefined, "error"), R.err("error"));
+  assertEquals(R.fromFalsy(false, "error"), R.err("error"));
+  assertEquals(R.fromFalsy("", "error"), R.err("error"));
+});
