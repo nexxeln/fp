083332567e240cf90a20fd06cc76b839e75fff29
Shoubhit Dash <shoubhit2005@gmail.com>
2023-02-18 09:37:48 +0530

    +(array): forgot to add tsdoc for all

--- src/Array.ts
+++ src/Array.ts
@@ -1,3 +1,26 @@
+/**
+ * Returns true if all elements in the array match the predicate, false otherwise.
+ *
+ * @param array - The array to operate on
+ * @param predicate - The predicate function to run on each element
+ *
+ * @example
+ * ```
+ * const x = pipe(
+ *   [1, 2, 3, 4, 5],
+ *   A.all((n) => n > 0)
+ * );
+ *
+ * const y = pipe(
+ *   ["hi", "hello", "howdy", "bye", "hey there"],
+ *   A.all((str) => str.startsWith("h"))
+ * );
+ *
+ *
+ * assertEquals(x, true);
+ * assertEquals(y, false);
+ * ```
+ */
 export function all<T>(array: T[], predicate: (value: T) => boolean): boolean;
 export function all<T>(
   predicate: (value: T) => boolean
