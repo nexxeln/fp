f69b0afe335a81de570fa254d0640b346f99100c
Shoubhit Dash <shoubhit2005@gmail.com>
2023-02-19 15:10:56 +0530

    +(docs): init

---
+++ docs/app.config.ts
@@ -0,0 +1,18 @@
+export default defineAppConfig({
+  docus: {
+    title: "fp_",
+    description: "A functional programming library for Deno",
+    socials: {
+      github: "nexxeln/fp",
+    },
+    aside: {
+      level: 0,
+      exclude: [],
+    },
+    header: {
+      showLinkIcon: true,
+      exclude: [],
+    },
+    footer: {},
+  },
+});
