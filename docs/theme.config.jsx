11bd80760edbbbdc7a8750f7714836ff85080e9b
Shoubhit Dash <shoubhit2005@gmail.com>
2023-02-20 15:51:05 +0530

    +(docs): add some metadata

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useRouter } from "next/router";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  logo: <span>fp_</span>,
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s • fp_",
+      };
+    } else {
+      return {
+        title: "fp_",
+      };
+    }
+  },
+  logo: <strong>fp_</strong>,
+  docsRepositoryBase: "https://github.com/nexxeln/fp/docs/pages",
+  feedback: {
+    content: null,
+  },
   project: {
     link: "https://github.com/nexxeln/fp",
   },
